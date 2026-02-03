1b6178674083a820d5003515e0d796f5a48c0780
ErickFloresOvando <erickfloresovando@gmail.com>
2026-02-02 20:14:32 -0600

    I have implemented GET,POST,PUT and DELET methods using RestAssured

---
+++ src/test/java/web/app/api/Student.java
@@ -0,0 +1,13 @@
+package web.app.api;
+
+public class Student {
+    String firstName;
+    String lastName;
+    String email;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
